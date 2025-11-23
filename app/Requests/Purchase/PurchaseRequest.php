1e57a2b887ccd36557ecb04154ededfd65328edb
ThantZinSwe <thantzinswe01@gmail.com>
2025-11-23 12:25:48 +0900

    fix: bug

--- app/Requests/Purchase/PurchaseRequest.php
+++ app/Requests/Purchase/PurchaseRequest.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Requests\Purchase;
 
+use App\Middleware\Api\AuthMiddleware;
 use Core\FormRequest;
 
 class PurchaseRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return !is_admin();
+        return !is_admin() || AuthMiddleware::user()->role === 'user';
     }
 
     public function rules(): array
